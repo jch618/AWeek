582e92d9623072607c60bbafc01d0ba63076c614
hyunseo <hansx2079@naver.com>
2025-08-07 20:09:49 +0900

    [UI System] Local-PlayerController 연결 Delegate 및 Policy System

--- Plugins/CommonGame/Source/CommonGame/Private/GameUIPolicy.cpp
+++ Plugins/CommonGame/Source/CommonGame/Private/GameUIPolicy.cpp
@@ -2,4 +2,97 @@
 
 
 #include "GameUIPolicy.h"
+#include "CommonLocalPlayer.h"
+#include "PrimaryGameLayout.h"
 
+UPrimaryGameLayout* UGameUIPolicy::GetRootLayout(const UCommonLocalPlayer* LocalPlayer) const
+{
+	return nullptr;
+}
+
+TSubclassOf<UPrimaryGameLayout> UGameUIPolicy::GetLayoutWidgetClass(UCommonLocalPlayer* LocalPlayer)
+{
+	return LayoutClass.LoadSynchronous();
+}
+
+void UGameUIPolicy::CreateLayoutWidget(UCommonLocalPlayer* LocalPlayer)
+{
+	if (APlayerController* PlayerController = LocalPlayer->GetPlayerController(GetWorld()))
+	{
+		TSubclassOf<UPrimaryGameLayout> LayoutWidgetClass = GetLayoutWidgetClass(LocalPlayer);
+		if (ensure(LayoutWidgetClass) && !LayoutWidgetClass->HasAnyClassFlags(CLASS_Abstract))
+		{
+			UPrimaryGameLayout* NewLayoutObject = CreateWidget<UPrimaryGameLayout>(PlayerController, LayoutWidgetClass);
+
+			RootViewportLayouts.Emplace(LocalPlayer, NewLayoutObject, true);
+			AddLayoutToViewport(LocalPlayer, NewLayoutObject);
+		}
+	}
+}
+
+void UGameUIPolicy::AddLayoutToViewport(UCommonLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout)
+{
+	Layout->SetPlayerContext(FLocalPlayerContext(LocalPlayer));
+	Layout->AddToPlayerScreen(1000);
+}
+
+void UGameUIPolicy::RemoveLayoutFromViewport(UCommonLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout)
+{
+	TWeakPtr<SWidget> LayoutSlateWidget = Layout->GetCachedWidget();
+	if (LayoutSlateWidget.IsValid())
+	{
+		Layout->RemoveFromParent();
+	}
+}
+
+void UGameUIPolicy::NotifyPlayerAdded(UCommonLocalPlayer* LocalPlayer)
+{
+	LocalPlayer->OnPlayerControllerSet.AddWeakLambda(this, [this](UCommonLocalPlayer* LocalPlayer, APlayerController* PlayerController)
+	{
+		NotifyPlayerRemoved(LocalPlayer);
+
+		if (FRootViewportLayoutInfo* LayoutInfo = RootViewportLayouts.FindByKey(LocalPlayer))
+		{
+			AddLayoutToViewport(LocalPlayer, LayoutInfo->RootLayout);
+			LayoutInfo->bAddedToViewport = true;
+		}
+		else
+		{
+			CreateLayoutWidget(LocalPlayer);
+		}
+	});
+	
+	if (FRootViewportLayoutInfo* LayoutInfo = RootViewportLayouts.FindByKey(LocalPlayer))
+	{
+		AddLayoutToViewport(LocalPlayer, LayoutInfo->RootLayout);
+		LayoutInfo->bAddedToViewport = true;
+	}
+	else
+	{
+		CreateLayoutWidget(LocalPlayer);
+	}
+}
+
+void UGameUIPolicy::NotifyPlayerRemoved(UCommonLocalPlayer* LocalPlayer)
+{
+	if (FRootViewportLayoutInfo* LayoutInfo = RootViewportLayouts.FindByKey(LocalPlayer))
+	{
+		RemoveLayoutFromViewport(LocalPlayer, LayoutInfo->RootLayout);
+		LayoutInfo->bAddedToViewport = false;
+	}
+}
+
+void UGameUIPolicy::NotifyPlayerDestroyed(UCommonLocalPlayer* LocalPlayer)
+{
+	NotifyPlayerRemoved(LocalPlayer);
+
+	LocalPlayer->OnPlayerControllerSet.RemoveAll(this);
+
+	const int32 LayoutInfoIndex = RootViewportLayouts.IndexOfByKey(LocalPlayer);
+	if (LayoutInfoIndex != INDEX_NONE)
+	{
+		UPrimaryGameLayout* Layout = RootViewportLayouts[LayoutInfoIndex].RootLayout;
+		RootViewportLayouts.RemoveAt(LayoutInfoIndex);
+		RemoveLayoutFromViewport(LocalPlayer, Layout);
+	}
+}
--- Plugins/CommonGame/Source/CommonGame/Public/GameUIPolicy.h
+++ Plugins/CommonGame/Source/CommonGame/Public/GameUIPolicy.h
@@ -3,8 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "GameUIPolicy.generated.h"
+
+class UCommonLocalPlayer;
+class UPrimaryGameLayout;
+
+USTRUCT()
+struct FRootViewportLayoutInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Transient)
+	TObjectPtr<ULocalPlayer> LocalPlayer = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UPrimaryGameLayout> RootLayout = nullptr;
+
+	UPROPERTY(Transient)
+	bool bAddedToViewport = false;
+
+	bool operator==(const ULocalPlayer* OtherLocalPlayer) const
+	{
+		return LocalPlayer == OtherLocalPlayer;
+	}
+
+	FRootViewportLayoutInfo()
+	{
+	}
+
+	FRootViewportLayoutInfo(ULocalPlayer* InLocalPlayer, UPrimaryGameLayout* InRootLayout, bool bIsInViewport)
+		: LocalPlayer(InLocalPlayer)
+		  , RootLayout(InRootLayout)
+		  , bAddedToViewport(bIsInViewport)
+	{
+	}
+};
 
 /**
  * 
@@ -13,5 +47,23 @@ UCLASS(Abstract, Blueprintable)
 class COMMONGAME_API UGameUIPolicy : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	UPrimaryGameLayout* GetRootLayout(const UCommonLocalPlayer* LocalPlayer) const;
+	TSubclassOf<UPrimaryGameLayout> GetLayoutWidgetClass(UCommonLocalPlayer* LocalPlayer);
 	
+	void CreateLayoutWidget(UCommonLocalPlayer* LocalPlayer);
+
+	void AddLayoutToViewport(UCommonLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout);
+	void RemoveLayoutFromViewport(UCommonLocalPlayer* LocalPlayer, UPrimaryGameLayout* Layout);
+
+	void NotifyPlayerAdded(UCommonLocalPlayer* LocalPlayer);
+	void NotifyPlayerRemoved(UCommonLocalPlayer* LocalPlayer);
+	void NotifyPlayerDestroyed(UCommonLocalPlayer* LocalPlayer);
+
+	UPROPERTY(EditAnywhere)
+	TSoftClassPtr<UPrimaryGameLayout> LayoutClass;
+
+	UPROPERTY(Transient)
+	TArray<FRootViewportLayoutInfo> RootViewportLayouts;
 };

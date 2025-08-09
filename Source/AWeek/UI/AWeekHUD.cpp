067b80f3c56f90887c42aa9b0540975d1db3db06
hyunseo <hansx2079@naver.com>
2025-08-09 16:46:10 +0900

    [UI System] AWeekHUD GameFrameWork Receiver 등록

--- Source/AWeek/UI/AWeekHUD.cpp
+++ Source/AWeek/UI/AWeekHUD.cpp
@@ -3,6 +3,27 @@
 
 #include "AWeekHUD.h"
 
+#include "Components/GameFrameworkComponentManager.h"
+
 AAWeekHUD::AAWeekHUD(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
 {
+}
+
+void AAWeekHUD::PreInitializeComponents()
+{
+	Super::PreInitializeComponents();
+
+	UGameFrameworkComponentManager::AddGameFrameworkComponentReceiver(this);
+}
+
+void AAWeekHUD::BeginPlay()
+{
+	UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(this, UGameFrameworkComponentManager::NAME_GameActorReady);
+	Super::BeginPlay();
+}
+
+void AAWeekHUD::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UGameFrameworkComponentManager::RemoveGameFrameworkComponentReceiver(this);
+	Super::EndPlay(EndPlayReason);
 }
--- Source/AWeek/UI/AWeekHUD.h
+++ Source/AWeek/UI/AWeekHUD.h
@@ -15,4 +15,8 @@ class AWEEK_API AAWeekHUD : public AHUD
 	GENERATED_BODY()
 public:
 	AAWeekHUD(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void PreInitializeComponents() override;
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 };

7269d90ca0f498d5f3ad03c89fab54b965256128
hyunseo <hansx2079@naver.com>
2025-08-24 17:02:52 +0900

    [UI System] Async Blueprint전용 Push Widget

---
+++ Plugins/CommonGame/Source/CommonGame/Public/AsyncAction_PushContentToLayerForPlayer.h
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/CancellableAsyncAction.h"
+#include "GameplayTagContainer.h"
+#include "UObject/SoftObjectPtr.h"
+
+#include "AsyncAction_PushContentToLayerForPlayer.generated.h"
+
+class APlayerController;
+class UCommonActivatableWidget;
+class UObject;
+struct FFrame;
+struct FStreamableHandle;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPushContentToLayerForPlayerAsyncDelegate, UCommonActivatableWidget*, UserWidget);
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class COMMONGAME_API UAsyncAction_PushContentToLayerForPlayer : public UCancellableAsyncAction
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	virtual void Cancel() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta=(WorldContext = "WorldContextObject", BlueprintInternalUseOnly="true"), Category = "Global UI Extensions")
+	static UAsyncAction_PushContentToLayerForPlayer* PushContentToLayerForPlayer(APlayerController* OwningPlayer, UPARAM(meta = (AllowAbstract=false)) TSoftClassPtr<UCommonActivatableWidget> WidgetClass, UPARAM(meta = (Categories = "UI.Layer")) FGameplayTag LayerName, bool bSuspendInputUntilComplete = true);
+
+	virtual void Activate() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FPushContentToLayerForPlayerAsyncDelegate AfterPush;
+
+private:
+	FGameplayTag LayerName;
+	bool bSuspendInputUntilComplete = true;
+	TWeakObjectPtr<APlayerController> OwningPlayerPtr;
+	TSoftClassPtr<UCommonActivatableWidget> WidgetClass;
+
+	TSharedPtr<FStreamableHandle> StreamingHandle;
+};

8c8fb6b24a437d3ce49e3952a18aa67e1d7f7b63
Chanha Jung <jch618@naver.com>
2025-09-02 12:48:42 +0900

    Feat: add chest class

---
+++ Source/AWeek/World/AWeekChest.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AWeek/Interfaces/AWeekInteractionInterface.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AWeekChest.generated.h"
+
+class AAWeekPlayerCharacter;
+struct FAWeekItemSlot;
+class UAWeekInventoryComponent;
+class USphereComponent;
+
+UCLASS()
+class AWEEK_API AAWeekChest : public AActor, public IAWeekInteractionInterface
+{
+	GENERATED_BODY()
+
+public:
+	AAWeekChest();
+
+	void BeginFocus() override;
+	void EndFocus() override;
+	void BeginInteract() override;
+	void EndInteract() override;
+	void Interact(TObjectPtr<AAWeekPlayerCharacter> PlayerCharacter) override;
+
+protected:
+	void BeginPlay() override;
+
+private:
+	//================================================================
+	//	PROPERTIES & VARIABLES
+	//================================================================
+	UPROPERTY(VisibleAnywhere, Category = "Chest")
+	TObjectPtr<AAWeekPlayerCharacter> PlayerCharacter;
+
+	UPROPERTY(VisibleAnywhere, Category = "Chest")
+	TObjectPtr<UAWeekInventoryComponent> InventoryComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Chest")
+	TObjectPtr<UStaticMeshComponent> ChestMesh;
+	
+	UPROPERTY()
+	TObjectPtr<USphereComponent> ChestExitSphere;
+	
+	UPROPERTY(EditAnywhere, Category = "Chest | Initialization")
+	float ChestExitRadius;
+
+	//================================================================
+	//	FUNCTIONS
+	//================================================================
+	UFUNCTION()
+	void OnChestRadiusExit(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+};

3be424309030708f46a7fa631dae9152e50e77fd
Chanha Jung <jch618@naver.com>
2025-09-02 12:38:30 +0900

    Feat: make controller control ui logic
    Make controller serve role of controller.

--- Source/AWeek/Player/AWeekPlayerController.cpp
+++ Source/AWeek/Player/AWeekPlayerController.cpp
@@ -3,8 +3,12 @@
 
 #include "AWeekPlayerController.h"
 #include "../UI/AWeekUserWidget.h"
+#include "AWeek/UI/AWeekInventoryMainPanel.h"
+#include "AWeek/UI/Interaction/AWeekInteractionWidget.h"
 
-AAWeekPlayerController::AAWeekPlayerController(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+#include "CommonUIExtensions.h"
+
+AAWeekPlayerController::AAWeekPlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer), bIsChestOpen(false)
 {
 }
 
@@ -24,4 +28,132 @@ void AAWeekPlayerController::BeginPlay()
 			MainWidget->AddToViewport();
 		}
 	}
+
+	check(InventoryMainPanelClass);
+	check(InteractionWidgetClass);
+
+	if (InventoryMainPanelClass)
+	{
+		InventoryMainPanelWidget = Cast<UAWeekInventoryMainPanel, UCommonActivatableWidget>(
+			UCommonUIExtensions::PushContentToLayer_ForPlayer(GetLocalPlayer(),
+			FGameplayTag::RequestGameplayTag("UI.Layer.GameMenu"), InventoryMainPanelClass));
+		InventoryMainPanelWidget->InitializeInventoryMainPanel();
+		InventoryMainPanelWidget->DeactivateWidget();
+	}
+}
+
+// =====================================================
+// INVENTORY SYSTEM
+// =====================================================
+
+void AAWeekPlayerController::ShowMainPanel()
+{
+	if (InventoryMainPanelClass)
+	{
+		InventoryMainPanelWidget = Cast<UAWeekInventoryMainPanel, UCommonActivatableWidget>(
+			UCommonUIExtensions::PushContentToLayer_ForPlayer(GetLocalPlayer(),
+				FGameplayTag::RequestGameplayTag("UI.Layer.GameMenu"), InventoryMainPanelClass));
+	}
+}
+void AAWeekPlayerController::HideMainPanel()
+{
+	if (InventoryMainPanelWidget)
+	{
+		InventoryMainPanelWidget->DeactivateWidget();
+	}
+}
+
+void AAWeekPlayerController::ToggleMainPanel()
+{
+	if (!IsValid(InventoryMainPanelWidget) || !InventoryMainPanelWidget->IsActivated())
+	{
+		ShowMainPanel();
+		SetShowMouseCursor(true);
+	}
+	else
+	{
+		if (bIsChestOpen)
+		{
+			DeactivateChestInventory();
+		}
+		HideMainPanel();
+		SetShowMouseCursor(false);
+	}
+}
+
+void AAWeekPlayerController::ToggleChestInventory(TObjectPtr<UAWeekInventoryComponent> ChestInventory)
+{
+	if (!bIsChestOpen)
+	{
+		if (!IsValid(InventoryMainPanelWidget) || !InventoryMainPanelWidget->IsActivated())
+		{
+			ShowMainPanel();
+			SetShowMouseCursor(true);
+		}
+		ActivateChestInventory(ChestInventory);
+	}
+	else
+	{
+		DeactivateChestInventory();
+		HideMainPanel();
+		SetShowMouseCursor(false);
+	}
+}
+
+
+void AAWeekPlayerController::ShowCrosshair()
+{
+	if (CrosshairWidget)
+	{
+		CrosshairWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void AAWeekPlayerController::HideCrosshair()
+{
+	if (CrosshairWidget)
+	{
+		CrosshairWidget->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+void AAWeekPlayerController::ShowInteractionWidget()
+{
+	if (InteractionWidgetClass)
+	{
+		InteractionWidget = Cast<UAWeekInteractionWidget, UCommonActivatableWidget>(
+			UCommonUIExtensions::PushContentToLayer_ForPlayer(GetLocalPlayer(),
+				FGameplayTag::RequestGameplayTag("UI.Layer.Game"), InteractionWidgetClass));
+	}
+}
+void AAWeekPlayerController::HideInteractionWidget()
+{
+	if (InteractionWidget)
+	{
+		InteractionWidget->DeactivateWidget();
+	}
+}
+void AAWeekPlayerController::UpdateInteractionWidget(const FAWeekInteractableData* InteractableData)
+{
+	if (InteractionWidget == nullptr || !InteractionWidget->IsActivated())
+	{
+		if (InteractionWidgetClass)
+		{
+			ShowInteractionWidget();
+		}
+
+		InteractionWidget->UpdateWidget(InteractableData);
+	}
+}
+
+void AAWeekPlayerController::ActivateChestInventory(TObjectPtr<UAWeekInventoryComponent> ChestInventory)
+{
+	bIsChestOpen = true;
+	InventoryMainPanelWidget->ActivateChestInventory(ChestInventory);
+}
+
+void AAWeekPlayerController::DeactivateChestInventory()
+{
+	bIsChestOpen = false;
+	InventoryMainPanelWidget->DeActivateChestInventory();
 }
--- Source/AWeek/Player/AWeekPlayerController.h
+++ Source/AWeek/Player/AWeekPlayerController.h
@@ -7,6 +7,10 @@
 #include "GameFramework/PlayerController.h"
 #include "AWeekPlayerController.generated.h"
 
+class UAWeekInventoryMainPanel;
+class UAWeekInteractionWidget;
+struct FAWeekInteractableData;
+class UAWeekInventoryComponent;
 /**
  * 
  */
@@ -17,9 +21,60 @@ class AWEEK_API AAWeekPlayerController : public ACommonPlayerController
 public:
 	AAWeekPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
+	// =====================================================
+	// INVENTORY SYSTEM
+	// =====================================================
+
+	//================================================================
+	//	PROPERTIES & VARIABLES
+	//================================================================
+	UPROPERTY(EditDefaultsOnly, Category = "Widgets")
+	TSubclassOf<UAWeekInventoryMainPanel> InventoryMainPanelClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Widgets")
+	TSubclassOf<UAWeekInteractionWidget> InteractionWidgetClass;
+
+	//UPROPERTY(EditDefaultsOnly, Category = "Widgets")
+	//TSubclassOf<UUserWidget> CrosshairWidgetClass;
+
+	//================================================================
+	//	FUNCTIONS
+	//================================================================
+
+	void ShowMainPanel();
+	void HideMainPanel();
+	void ToggleMainPanel();
+	void ToggleChestInventory(TObjectPtr<UAWeekInventoryComponent> ChestInventory);
+
+	void ShowCrosshair();
+	void HideCrosshair();
+
+	void ShowInteractionWidget();
+	void HideInteractionWidget();
+	void UpdateInteractionWidget(const FAWeekInteractableData* InteracableData);
+
+	void ActivateChestInventory(TObjectPtr<UAWeekInventoryComponent> ChestInventory);
+	void DeactivateChestInventory();
+
 protected:
 	TSubclassOf<UUserWidget> mMainWidget;
 
 protected:
 	virtual void BeginPlay() override;
+
+protected:
+	//================================================================
+	//	PROPERTIES & VARIABLES
+	//================================================================
+	UPROPERTY()
+	TObjectPtr<UAWeekInventoryMainPanel> InventoryMainPanelWidget;
+
+	UPROPERTY()
+	TObjectPtr<UAWeekInteractionWidget> InteractionWidget;
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> CrosshairWidget;
+
+private:
+	bool bIsChestOpen;
 };
